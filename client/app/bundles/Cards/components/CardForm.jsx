dbc7710d2cafcbccb7d2bdcab8975a7b44e66bb6
Molly Trombley-McCann <molly.kayline@gmail.com>
2017-07-03 21:51:49 -0700

    Style card form

--- client/app/bundles/Cards/components/CardForm.jsx
+++ client/app/bundles/Cards/components/CardForm.jsx
@@ -10,16 +10,19 @@ export default class CardForm extends React.Component {
   render() {
     return (
       <div>
-        <form action={"/card_templates/" + this.props.template.id +"/cards"} method="post">
+        <form className="pl" action={"/card_templates/" + this.props.template.id +"/cards"} method="post">
           <input type='hidden' name='authenticity_token' value={this.props.auth_token} />
+          <div className="form-group">
+            <label>Selected Greeting</label>
+            <div className="form-control">{this.props.template.greeting}</div>
+          </div>
 
-          <label>Selected Greeting</label>
-          <div>{this.props.template.greeting}</div>
+          <div className="form-group">
+            <label htmlFor="custom_message">Custom Message</label>
+            <input className="form-control" id="custom_message" name="custom_message" type="text"></input>
+          </div>
 
-          <label htmlFor="custom_message">Custom Message</label>
-          <input id="custom_message" name="custom_message" type="text"></input>
-
-          <input type="submit" value="Send Card"></input>
+          <input className="btn btn-default" type="submit" value="Send Card"></input>
         </form>
       </div>
     );
