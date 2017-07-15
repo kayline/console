76d48ec8d82c00616bb87cc5b6adaadf4088aacb
Ransom <ransom.byers@gmail.com>
2017-07-14 18:01:28 -0700

    Add card test examples to admin feature tests

--- spec/features/orders_admin_spec.rb
+++ spec/features/orders_admin_spec.rb
@@ -2,6 +2,26 @@ require 'rails_helper'
 
 describe 'Orders admin', js: true do
   let!(:card_template) { CardTemplate.create!(greeting: 'Tell it to the judge') }
+  let!(:first_card) { Card.create(
+      card_template_id: card_template.id,
+      custom_message: 'some text',
+      signature: 'a signature',
+      recipient_name: 'your friend',
+      street_address: 'Here',
+      city: 'There',
+      state: 'CA',
+      zip_code: '55555',
+  )}
+  let!(:second_card) { Card.create(
+      card_template_id: card_template.id,
+      custom_message: 'happy moving away day',
+      signature: 'the cat next door',
+      recipient_name: 'doggo',
+      street_address: '123 4 St',
+      city: 'Wherever',
+      state: 'CA',
+      zip_code: '55555',
+  )}
 
   it 'displays an admin page' do
     visit '/admin'
