41f11c9673ac1dc9e207a84be7e22b8ef3c300c2
Ransom <ransom.byers@gmail.com>
2017-07-21 16:53:11 -0700

    Display the active card's details on the admin page

--- client/app/bundles/Cards/components/Admin.jsx
+++ client/app/bundles/Cards/components/Admin.jsx
@@ -1,6 +1,7 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import CardList from './CardList';
+import CardDetails from './CardDetails';
 
 export default class Admin extends React.Component {
   static propTypes = {
@@ -10,25 +11,38 @@ export default class Admin extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = { active_card: this.props.cards[0] }
-    this.state.changeActiveCard = this.changeActiveCard.bind(this)
+    this.state = {
+      cards: this.props.cards,
+      active_card: this.props.cards[0]
+    };
   }
 
   changeActiveCard(new_active_card_id) {
-    console.log('changing active card');
-    console.log(new_active_card_id);
+    var that = this;
+    if (new_active_card_id != this.state.active_card.id) {
+      var newly_active_card = that.state.cards.find( function(card) {
+        return new_active_card_id == card.id;
+      });
+      that.setState({ active_card: newly_active_card });
+    }
   }
 
   render() {
     return (
       <div className="container">
         <h3> Admin </h3>
-        <CardList
-          templates = {this.props.templates}
-          cards = {this.props.cards}
-          active_card_id = {this.state.active_card.id}
-          change_active_card = {this.state.changeActiveCard}
-        />
+        <div>
+          <CardList
+            templates = {this.props.templates}
+            cards = {this.state.cards}
+            active_card_id = {this.state.active_card.id}
+            change_active_card = {this.changeActiveCard.bind(this)}
+          />
+          <CardDetails
+            card = {this.state.active_card}
+            greeting = "placeholder greeting"
+          />
+        </div>
       </div>
     );
   }
